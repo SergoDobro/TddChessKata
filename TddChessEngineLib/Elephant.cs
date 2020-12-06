d2d4beeed46434680a1a82d1ee03ae93e7cf1c50
SergoDobro <optimgrey@yandex.ru>
2020-12-06 12:07:35 +0300

    elephant commit

--- TddChessEngineLib/Elephant.cs
+++ TddChessEngineLib/Elephant.cs
@@ -1,23 +1,50 @@
 using System;
 namespace TddChessEngineLib
 {
-    class Elephant
+    public enum letters { A,B,C,D,E,F,G,H }
+    public class Elephant
     {
         public string CurrentPosition { get; private set; }
-        
         public Elephant(string newPosition){
             int pos =  Convert.ToInt32(newPosition[1].ToString());
-            char let = newPosition[0];
-            if (let!='A'&&let!='B'&&let!='C'&&let!='D'&&let!='E'&&let!='F'&&let!='G'&&let!='H'){
+            string let = newPosition[0].ToString();
+            if (let!="A"&&let!="B"&&let!="C"&&let!="D"&&let!="E"&&let!="F"&&let!="G"&&let!="H"){
                 throw new ArgumentException("Wrong letter!");
             }
-            if (pos>8 || pos<0){
+            if (pos>8 || pos<1){
                 throw new ArgumentException("Wrong number!");
             }
+            CurrentPosition = let+pos;
         } 
-
+        public letters GetLetter(string let){
+            switch (let){
+                case "A":
+                    return letters.A;
+                case "B":
+                    return letters.B;
+                case "C":
+                    return letters.C;
+                case "D":
+                    return letters.D;
+                case "E":
+                    return letters.E;
+                case "F":
+                    return letters.F;
+                case "G":
+                    return letters.G;
+                case "H":
+                    return letters.H;
+                default:
+                    throw new ArgumentException("Wrong letter!");
+            }
+        }
         public void Move(string newPosition){
-            
+            int pos =  Convert.ToInt32(newPosition[1].ToString());
+            letters let = GetLetter(newPosition[0].ToString());
+            letters curlet = GetLetter(CurrentPosition[0].ToString());
+            //if (int.Parse(curlet)-int.Parse(let){
+
+            //}
         }
     }
 }
--- TddChessEngineLibTests/ElephantTests.cs
+++ TddChessEngineLibTests/ElephantTests.cs
@@ -9,13 +9,20 @@ namespace TddChessEngineLibTests
         [Fact]
         public void WhenCreatingElephantOnE2_ThenItCurrentPositionE2()
         {
-            Elephant el = new Elephant();
+            Elephant el = new Elephant("E2");
+            Assert.Equal("E2", el.CurrentPosition);
         }
 
         [Fact]
         public void WhenTryCreatingElephantOnE9_ThenItThrowsArgumentException()
         {
-
+            Assert.Throws<ArgumentException>(() => new Elephant("E9"));
         }
+        [Fact]
+        public void WhenTryCreatingElephantOnP2_ThenItThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new Elephant("P2"));
+        }
+
     }
 }
